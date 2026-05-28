0d99d26dd7557a6ec5f7ee9bbb6048c2a87bc4f7
Jean <55226561+JeanChuang-learning@users.noreply.github.com>
2026-05-28 10:59:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ for row_start in range(0, len(visible_keys), 3):
             end_h, end_m = map(int, s.get("end_time", "22:00")[:5].split(":"))
             session_end_dt = datetime.combine(s_date_obj, datetime.min.time()).replace(hour=end_h, minute=end_m)
             is_ended = datetime.now() > session_end_dt
-            is_pause = datetime.now) + timedelta(days=7) > session_end_dt
+            is_pause = datetime.now() + timedelta(days=7) < session_end_dt
         except Exception:
             is_ended = s_date_obj < today_date
 
