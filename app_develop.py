4972d6fc14efae7c25c3a980dd9c426b98d6e7c1
Jean <55226561+JeanChuang-learning@users.noreply.github.com>
2026-06-04 16:18:44 +0800

    Update app_develop.py

--- app_develop.py
+++ app_develop.py
@@ -348,10 +348,8 @@ today_date = date.today()
 # ─────────────────────────
 # 標題
 # ─────────────────────────
-st.markdown("""
-    <h1 style='margin-bottom: 0px;'>🏸 信義羽球隊</h1> 
-    <h3 style='margin-top: 0px; color: #555555;'>年度會員招募中 !</h3> <p style='color: #888888; font-size: 16px;'>誠摯邀請熱愛羽球的夥伴加入我們</p>
-""", unsafe_allow_html=True)
+st.markdown("""<h1 style='margin-bottom: 0px;'>🏸 信義羽球隊</h1>""", unsafe_allow_html=True)
+st.markdown("""<h3>年度會員招募中！<span style='font-size: 16px; color: #666;'>誠摯邀請熱愛羽球的夥伴加入我們</span></h3>""", unsafe_allow_html=True)
 
 ann = get_announcement()
 if ann:
